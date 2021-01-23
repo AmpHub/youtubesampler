9960f036e317665525242075cf0e0e780939c2d3
matttt <matthew.martori@gmail.com>
2021-01-22 21:47:31 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
+const express = require('express')
+const ffmpeg = require('fluent-ffmpeg')
+const bodyParse = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParse.json())
+
+app.post('/download', (req, res) => {
+
+  const videoid = req.body.url.match(/watch\?v=.{1,11}/)[0].substring(8)
+  const video = youtubedl(videoid)
+  
+  // Will be called when the download starts.
+  video.on('info', (info) => {
+    console.log('Download started')
+    console.log('filename: ' + info._filename)
+    console.log('size: ' + info.size)
+
+    const title = info._filename.substring(0, 20)
+
+    res.attachment(`${title} - ${videoid}.mp3`);
+  
+    ffmpeg(video)
+      .format('mp3')
+      .output(res)
+      .on('end', function() {
+        console.log('Finished processing');
+      })
+      .run();
+  })
+  
+
+})
+
+app.use(express.static('static'))
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
